39387ca3cdf7066ff10f7f85881099cd7d9518d0
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-01-23 22:48:44 +0530

    Updated FirstLastPositon Program with another logic

--- Arrays/FirstLastPosition.java
+++ Arrays/FirstLastPosition.java
@@ -1,10 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
-
 
 /**
  *
@@ -24,56 +20,60 @@ public class FirstLastPosition {
         }
       System.out.println("Target:");
       int target=sc.nextInt();
-        int[] ans=firstLastPosition(arr, target);
+        int[] ans=searchRange(arr, target);
         for (int i = 0; i < ans.length; i++) {
             System.out.println(ans[i]);
             
         }
         
     }
-public static int[] firstLastPosition(int[] arr, int target) {
-        int first = findFirst(arr, target);
-        int last = findLast(arr, target);
-        return new int[]{first, last};
-}
-    public static int findFirst(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;        
-                high = mid - 1;   
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+public static int[] searchRange(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        int firstoccur=-1;
+        int lastoccur=-1;
+        
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target>nums[mid])
+            {
+                start=mid+1;
             }
-        }
-        return ans;
-    }
-
-    
-    public static int findLast(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;        
-                low = mid + 1;    
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            else if(target<nums[mid])
+            {
+                end=mid-1;
             }
+           
+            else if(target==nums[mid]){
+                firstoccur=mid;
+                end=mid-1;
+            }
+            
+          }
+          start=0;
+         end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target>nums[mid])
+            {
+                start=mid+1;
+            }
+            else if(target<nums[mid])
+            {
+                end=mid-1;
+            }
+           
+            else if(target==nums[mid]){
+               lastoccur=mid;
+                start=mid+1;
+            }
+            
+            
+          }
+        return new int[]{firstoccur,lastoccur};
         }
-        return ans;
-    }
 }
     
 
