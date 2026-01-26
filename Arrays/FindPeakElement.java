381c472e4557b9e8f1f723042e91ef3e1d0425d4
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-01-26 19:17:35 +0530

    Added a program for finding peak element by linear as well as binary Search

---
+++ Arrays/FindPeakElement.java
@@ -0,0 +1,36 @@
+
+
+/**
+ *
+ * @author Siddharth Valecha
+ */
+public class FindPeakElement {
+
+    
+    public static void main(String[] args) {
+       
+      int[] arr={1,2,1,3,5,6,4};
+      int ans=findPeakElement(arr);
+        System.out.println(ans);
+    
+    }
+    public static int findPeakElement(int[] arr) {
+       int start=0;
+       int end=arr.length-1;
+       while(start!=end)
+       {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])
+            {
+                end=mid;
+            }
+            else if(arr[mid]<arr[mid+1])
+            {
+                start=mid+1;
+            }
+            
+       }
+       return start;
+    }
+    
+}
