70d53a25fca85d8af5d3b8eb48c8f8f6217266be
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-03-17 23:28:48 +0530

    Added a program to find sum of digits of a given number

---
+++ Loops/SumOfDigits.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dsa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Siddharth Valecha
+ */
+public class SumOfDigits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Number");
+        int n=sc.nextInt();
+        int ans=0;
+        while(n>0)
+        {
+            ans+=n%10;
+            n/=10;
+        }
+        System.out.println("Sum is:"+ans);
+    }
+    
+}
