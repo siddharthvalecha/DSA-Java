3c4fd75f8a3aee0ec6aec4307b1010cf2ca895ee
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-07-14 22:52:53 +0530

    Added a solution for the problem named Pow(x,n)

---
+++ Arrays/Recursion/Powx.java
@@ -0,0 +1,51 @@
+/*
+50. Pow(x, n) https://leetcode.com/problems/powx-n/description/
+
+ */
+package dsa.Recursion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Powx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter x");
+        double x=sc.nextDouble();
+        System.out.println("Enter n");
+        int n=sc.nextInt();
+        double ans=myPow(x,n);
+        System.out.println("Answer is: "+ans);
+    }
+     public static double helper(double x, long exponent) {
+        if (exponent == 0) {
+            return 1.0;
+        }
+
+        double half = helper(x, exponent / 2);
+
+        if (exponent % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+    public static double myPow(double x, int n) {
+         long exponent = n;
+
+        if (exponent < 0) {
+            exponent = -exponent;
+            return 1.0 / helper(x, exponent);
+        }
+
+        return helper(x, exponent);
+        
+        }  
+}
