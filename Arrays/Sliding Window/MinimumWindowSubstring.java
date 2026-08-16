57ba320cfcd1e379654adf5586d9d30fc1b883ed
Siddharth Valecha <siddharthvalecha319b@gmail.com>
2026-08-16 20:39:22 +0530

    Added a solution for the problem named  MinimumWindowSubstring

--- Arrays/Sliding Window/MinimumWindowSubstring.java
+++ Arrays/Sliding Window/MinimumWindowSubstring.java
@@ -4,6 +4,7 @@
 package dsa.SlidingWindow;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -15,51 +16,62 @@ public class MinimumWindowSubstring {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);    
           System.out.println("Enter The String ");
-        String str=sc.next();
-      
+        String str1=sc.next();
+        String str2=sc.next();
         
-        int ans=solu(str);
+       String ans=minWindow(str1,str2);
         System.out.println("Answer is :"+ ans);
     }
-    public static int solu(String t){
-         int m=t.length();
-        int high=0;
-        int low=0;
-        HashMap<Character,Integer> freqt=new HashMap<>();
-        for(int i=0;i<m;i++){
-            char charAti=t.charAt(i);
-            freqt.put(charAti,freqt.getOrDefault(charAti,0)+1);
-            while()
-            }
-        System.out.println(freqt);
-        return 0;
-    }
-    public static int lengthOfLongestSubstring(String s) {
-        int n=s.length();
-        int low=0;
-        int high=0;
-        HashMap<Character,Integer> freq=new HashMap<>();
-        int res=Integer.MIN_VALUE;
-        for(high=0;high<n;high++){
-            char charAtHigh=s.charAt(high);
-            freq.put(charAtHigh,freq.getOrDefault(charAtHigh,0)+1);
-            int k=high-low+1;
-            while(freq.size()<k){
-                char charAtLow=s.charAt(low);
-                freq.put(charAtLow,freq.get(charAtLow)-1);
-                if(freq.get(charAtLow)==0){
-                    freq.remove(charAtLow);
-                }
-                low++;
-                k=high-low+1;
-            }
-            int len=high-low+1;
-            res=Math.max(len,res);
+    public static String minWindow(String s,String t){
             
+            int low=0;
+            int start=0;
+            int high=0;
+            int n=s.length();
+            int res=Integer.MAX_VALUE;
+            
+            HashMap<Character,Integer> freqs=new HashMap<>();
+            HashMap<Character,Integer> freqt=makeFreqt(t);
+            for(high=0;high<n;high++){
+                char charAtHigh=s.charAt(high);
+                freqs.put(charAtHigh, freqs.getOrDefault(charAtHigh,0)+1);
+                while(isValid(freqs,freqt)){
+                    int len=high-low+1;
+                    if(res>len){
+                        res=len;
+                        start=low;
+                    }
+                    char charAtLow=s.charAt(low);
+                    freqs.put(charAtLow,freqs.getOrDefault(charAtLow,0)-1);
+                    if(freqs.get(charAtLow)==0){
+                        freqs.remove(charAtLow);
+                    }
+                    low++;
+                }
+                
+            }
+            if(res==Integer.MAX_VALUE) return "";
+            
+            return s.substring(start,start+res);
+        
+    }
+        public static HashMap<Character,Integer> makeFreqt(String t){
+                HashMap<Character,Integer> freqt=new HashMap<>();
+                int n=t.length();
+                for (int i = 0; i < n; i++) {
+                char ch=t.charAt(i);
+                freqt.put(ch,freqt.getOrDefault(ch, 0)+1);
+                }
+                return freqt;
         }
-        if(res==Integer.MIN_VALUE){
-            res=0;
+        public static boolean isValid(HashMap<Character,Integer> freqs,HashMap<Character,Integer> freqt){
+            for(Map.Entry<Character,Integer> entry : freqt.entrySet()){
+                char ch=entry.getKey();
+                int reqfreq=entry.getValue();
+                if(freqs.getOrDefault(ch, 0)<reqfreq){
+                    return false;
+                }
+            }
+            return true;
         }
-        return res;
-    }    
 }
